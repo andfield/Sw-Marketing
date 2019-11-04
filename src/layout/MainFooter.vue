573b6138ba176a61de96d7d59d53828a5586121d
Sid <siddharththakur76@gmail.com>
2019-11-05 11:50:43 +1300

    initial push

---
+++ src/layout/MainFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer" :class="{ [`footer-${type}`]: type }">
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <span>Team Nerdling</span>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright">
+        &copy; {{ year }}, Designed by
+        Team Nerdlings.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  props: {
+    backgroundColor: String,
+    type: String
+  },
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
